5accd32942b9849a0aa68fc00e87fdf53a3ccf10
RSAPL <ramon.saucedo.s@protonmail.com>
2022-07-22 17:01:19 -0500

    Added basic functionality

--- game.js
+++ game.js
@@ -3,19 +3,78 @@ const getComputerChoice = () => {
     switch (choice){
         case 0:
             return "rock";
-            break;
         
         case 1:
             return "paper";
-            break;
     
         case 2:
             return "scissors";
-            break;
     }
 }
 
-const playGame = (userChoice, computerChoice) => {
+
+
+const decideWinner = (userChoice, computerChoice) => {
     if(userChoice === computerChoice) {
+        switch(userChoice) {
+            case "rock":
+                setTimeout(() => {document.getElementById('rock').innerHTML = '✊'},  700);
+                document.getElementById('rock').innerHTML = '🏳️';
+                break;
+
+            case "paper":
+                setTimeout(() => {document.getElementById('paper').innerHTML = '✋'},  700);
+                document.getElementById('paper').innerHTML = '🏳️';
+                break;
+
+            case "scissors":
+                setTimeout(() => {document.getElementById('scissors').innerHTML = '✌️'},  700);
+                document.getElementById('scissors').innerHTML = '🏳️';
+                break;
+        }
+    } else {
+        switch(userChoice) {
+            case "rock":
+                if(computerChoice === "scissors") {
+                    setTimeout(() => {document.getElementById('rock').innerHTML = '✊'},  700);
+                    document.getElementById('rock').innerHTML = '✔️';
+                    break;
+
+                } else if (computerChoice === "paper") {
+                    setTimeout(() => {document.getElementById('rock').innerHTML = '✊'},  700);
+                    document.getElementById('rock').innerHTML = '❌';
+                    break;
+                };
+                
+
+            case "paper":
+                if(computerChoice === "rock") {
+                    setTimeout(() => {document.getElementById('paper').innerHTML = '✋'},  700);
+                    document.getElementById('paper').innerHTML = '✔️';
+                    break;
+
+                } else if (computerChoice === "scissors") {
+                    setTimeout(() => {document.getElementById('paper').innerHTML = '✋'},  700);
+                    document.getElementById('paper').innerHTML = '❌';
+                    break;
+                };
+
+
+             case "scissors":
+                if(computerChoice === "paper") {
+                    setTimeout(() => {document.getElementById('scissors').innerHTML = '✌️'},  700);
+                    document.getElementById('scissors').innerHTML = '✔️';
+                    break;
+
+                } else if (computerChoice === "rock") {
+                    setTimeout(() => {document.getElementById('scissors').innerHTML = '✌️'},  700);
+                    document.getElementById('scissors').innerHTML = '❌';
+                    break;
+                };
+        }
     }
+}
+
+const playGame = () => {
+    let 
 }
